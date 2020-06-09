38aaefaeae9c2a33099888c0b2c3f118ab9a7336
Fengshuting2019 <319411187@qq.com>
2020-06-09 15:12:17 +0800

    添加本地仓库

---
+++ localgit/src/com/nnxy/control/UserControl.java
@@ -0,0 +1,14 @@
+package com.nnxy.control;
+
+public class UserControl {
+	public void addUser(int id, String username,String passwaord) {
+		
+	}
+	public void deleterUser(int id) {
+		
+	}
+	public void updateUser(int id, String newUsername,String newPasswaord) {
+		
+	}
+
+}
